293fe74717919981dbb1856c672dc54aa1097d60
Gabriela Carraro <gabrielacarraro2000@gmail.com>
2022-04-21 19:52:32 -0300

    finish login function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function send() {
+function usernamePasswordValidatior() {
     const username = document.querySelector("input[name='username']").value;
     const password = document.querySelector("input[name='password']").value;
 
@@ -15,6 +15,29 @@ function send() {
     };
 }
 
+function login(){
+    const usernamePasswordValidation = usernamePasswordValidatior();
+
+    if (usernamePasswordValidation.sucess) {
+        const userFund = table.find(function (row){
+            if (row.username == usernamePasswordValidation.username){
+                return true;
+            }
+
+            return false;
+        });
+
+        if (!userFund || userFund.password != usernamePasswordValidation.password) {
+            alert ("O usuário e senha informados não podem ser encontrados!");
+
+            return;
+        }
+
+        alert("Loboi");
+
+    }
+}
+
 function register() {
     const email = document.querySelector("input[name='email']").value;
     const fullname = document.querySelector("input[name='fullname']").value;
@@ -22,10 +45,12 @@ function register() {
     if (email.length < 6 || fullname.length < 6) {
         alert("É necessário ter mais que 6 caracteres");
 
-        return;
+        return{
+            sucess: false,
+        };
     }
     
-    const usernamePasswordValidation = send();
+    const usernamePasswordValidation = usernamePasswordValidatior();
 
     if (usernamePasswordValidation.sucess) {
         const data = {
@@ -36,14 +61,32 @@ function register() {
         };
 
         console.log(database(data));
+
+        return{
+            sucess: true,
+            data:data
+        };
     }
 
 }
 
-const table = []
+const table = [
+    {
+        email: "gabriela@liveonsolutions.com",
+        fullname: "Gabriela",
+        username: "Gabriela",
+        password: "123456",
+    },
+    {
+        email: "",
+        fullname: "",
+        username: "",
+        password: "",
+    },
+]
 
-function database(data) {
-    table.push(data);
+function database(argument){
+    table.push(argument);
 
     return table;
 }
